0e236a321667c72732070ada90b2f3c627eb8bd5
JosueGarrido <josue.garrido@epn.edu.ec>
2021-01-16 02:41:18 -0500

    login-register

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,11 @@
+// login.page.ts
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+import { AuthenticateService } from '../../services/authentication.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app'
+import "firebase/auth";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  
+  constructor(
+
+    private navCtrl: NavController,
+    private authService: AuthenticateService,
+    private formBuilder: FormBuilder,
+    private afAuth: AngularFireAuth
+
+  ) { }
 
   ngOnInit() {
+
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
+  }
+
+
+  validation_messages = {
+    'email': [
+      { type: 'required', message: 'El email es requerido.' },
+      { type: 'pattern', message: 'Por favor ingresar un email válido.' }
+    ],
+    'password': [
+      { type: 'required', message: 'La contraseña es requerida.' },
+      { type: 'minlength', message: 'Ingrese una contraseña de por lo menos 5 caracteres.' }
+    ]
+  };
+
+
+  loginUser(value) {
+    this.authService.loginUser(value)
+      .then(res => {
+        console.log(res);
+        this.errorMessage = "";
+        this.navCtrl.navigateForward('/dashboard');
+      }, err => {
+        this.errorMessage = err.message;
+      })
+  }
+
+  goToRegisterPage() {
+    this.navCtrl.navigateForward('/register');
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyC1kipFx6HJL6dl5M4jsUVaKBV0s-3M9NM",
+    authDomain: "examen-topicos.firebaseapp.com",
+    databaseURL: "https://examen-topicos-default-rtdb.firebaseio.com",
+    projectId: "examen-topicos",
+    storageBucket: "examen-topicos.appspot.com",
+    messagingSenderId: "573850596543",
+    appId: "1:573850596543:web:22971689be328e2008820a"
+  }};
 
 /*
  * For easier debugging in development mode, you can import the following file
